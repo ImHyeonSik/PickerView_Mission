c6c6d0835a917e1ecfff906a45883179f98f69f1
hyeonsik <dgt4862@gmail.com>
2022-05-01 00:51:27 +0900

    picker View 의 선택된 Text, Image 의 값을 보여주는 예제

--- PickerView_Mission/ViewController.swift
+++ PickerView_Mission/ViewController.swift
@@ -7,11 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    let MAX_ARRAY_COLUMN = 3
+    let PICKER_VIEW_COLUMN = 1
+    let PICKER_VIEW_HEIGHT: CGFloat = 80
+    var imageArray = [UIImage?]()
+    var imageFileName = ["1.png", "2.png", "3.png"]
+    
 
+    @IBOutlet var pickerImage: UIPickerView!
+    
+    @IBOutlet var lbImageFileName: UILabel!
+    
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for i in 0..<MAX_ARRAY_COLUMN {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        lbImageFileName.text = imageFileName[0]
+        imageView.image = imageArray[0]
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return imageFileName[row]
+//    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArray[row])
+        imageView.frame = CGRect(x:0, y:0, width: 100, height: 150)
+        
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // piker view 의 제목을 label 에 저장
+        lbImageFileName.text = imageFileName[row]
+        imageView.image = imageArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
     }
 
 
